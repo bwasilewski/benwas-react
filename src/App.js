5eaee9a28becff7a119aacfb129c66dc6ed451b9
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2018-03-19 11:25:12 -0500

    header and footer components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Header from './components/header/Header.js';
+import Footer from './components/footer/Footer.js';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +8,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="Header">
+                <h1 className="App-title">BenWas.com</h1>
+            </header>
+        );
+    }
+}
+
+export default Header;
