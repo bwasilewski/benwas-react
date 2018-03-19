1c9404100fb01e5c808a42b762848f96c9951e27
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2018-03-19 15:30:36 -0500

    more style adjustments

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,15 +5,16 @@ class Contact extends Page {
     render() {
         return (
             <div className="Page">
+                <h2>Contact Me</h2>
                 <p>There are many ways that you can contact me:</p>
 
-                <b>Direct</b>
+                <h3>Direct</h3>
                 <ul>
                     <li>Phone: (847) 721-9972</li>
                     <li>Email: <a href="mailto:benwasilewski@gmail.com">benwasilewski@gmail.com</a></li>
                 </ul>
 
-                <b>Social Media</b>
+                <h3>Social Media</h3>
 
                 <p>Find me on: <a href="http://github.com/bwasilewski">GitHub</a>, <a href="http://bitbucket.org/bwasilewski">BitBucket</a> and <a href="https://www.linkedin.com/in/benwasilewski">LinkedIn</a></p>
             </div>
